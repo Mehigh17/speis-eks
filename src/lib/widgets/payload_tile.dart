db680d4fe75567b3c03ed014f67337e2a03952e1
Mehigh17 <kazepworld@gmail.com>
2019-05-05 14:18:28 +0200

    Make payload tiles white with rounded borders

--- src/lib/widgets/payload_tile.dart
+++ src/lib/widgets/payload_tile.dart
@@ -12,27 +12,39 @@ class PayloadTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Theme.of(context).backgroundColor,
-      margin: EdgeInsets.all(10.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Text(
-            payload.id,
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.title,
-          ),
-          _evenRow(Text("Made by ${payload.manufacturer}")),
-          _oddRow(Text("Weight: ${payload.massKg}kg (${payload.massLbs}lbs)")),
-          _evenRow(Text("Is reused: ${payload.reused ? 'Yes' : 'No'}")),
-          _oddRow(Text("Payload type: ${payload.type}")),
-          _evenRow(Text("Nationality: ${payload.nationality}")),
-          _oddRow(Text("Customers: ${payload.customers.join(', ')}")),
-          _evenRow(Text("Orbit lifespan: ${payload.orbitParams.lifespan} years")),
-          _oddRow(Text("Orbit period: ${payload.orbitParams.period} minutes")),
-          _evenRow(Text("Orbit regime: ${payload.orbitParams.regime}"))
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(blurRadius: 1.0, color: Colors.black38)
         ],
+      ),
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              payload.id,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.title,
+            ),
+            _evenRow(Text("Made by ${payload.manufacturer}")),
+            _oddRow(
+                Text("Weight: ${payload.massKg}kg (${payload.massLbs}lbs)")),
+            _evenRow(Text("Is reused: ${payload.reused ? 'Yes' : 'No'}")),
+            _oddRow(Text("Payload type: ${payload.type}")),
+            _evenRow(Text("Nationality: ${payload.nationality}")),
+            _oddRow(Text("Customers: ${payload.customers.join(', ')}")),
+            _evenRow(
+                Text("Orbit lifespan: ${payload.orbitParams.lifespan} years")),
+            _oddRow(
+                Text("Orbit period: ${payload.orbitParams.period} minutes")),
+            _evenRow(Text("Orbit regime: ${payload.orbitParams.regime}"))
+          ],
+        ),
       ),
     );
   }
@@ -47,7 +59,6 @@ class PayloadTile extends StatelessWidget {
 
   Widget _oddRow(Widget child) {
     return Container(
-      color: Colors.black26,
       padding: EdgeInsets.all(2.5),
       child: child,
     );
